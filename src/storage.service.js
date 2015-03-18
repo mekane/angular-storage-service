f6b082fe335837dad04c12845c5b3275fbee11c6
Marty Kane <marty@pixotech.com>
2015-03-17 19:34:43 -0500

    Added source file

---
+++ src/storage.service.js
@@ -0,0 +1,90 @@
+angular.module('mekane')
+.service('storage', storageService);
+
+function storageService( $q, $http, node ){
+
+    var defaults = {
+        dbHost: 'http://couch-server:5984',
+        dbName: 'dbName',
+        design: '/_design/views/_view/'
+    };
+
+    return {
+        newMemoryStore: newMemoryStore,
+        newCouchStore: newCouchStore
+    };
+
+
+    function newMemoryStore(){
+        var memStore = {};
+
+        return {
+            save: saveInMemory,
+            load: loadFromMemory,
+            list: listFromMemory
+        };
+
+
+        function saveInMemory( data ){
+            return $q(function(resolve, reject){
+                var id = data.id || node.generateUuid();
+                data['id'] = id;
+                memStore[id] = data;
+                resolve(id);
+            });
+        }
+
+        function loadFromMemory( id ){
+            return $q(function(resolve, reject){
+                if ( memStore[id] ){
+                    resolve(memStore[id]);
+                }
+                else {
+                    reject('id '+id+' not found');
+                }
+            });
+        }
+
+        function listFromMemory(){
+            return $q(function(resolve, reject){
+                var result = Object.keys(memStore).map(function(key){
+                    return memStore[key];
+                });
+                resolve( result );
+            });
+        }
+    }
+
+    function newCouchStore( options ){
+        var opts = options || {};
+        var dbHost = opts.dbHost || defaults.dbHost;
+        var dbName = opts.dbName || defaults.dbName;
+        var design = opts.design || defaults.design;
+        var couchUrl = dbHost + '/' + dbName;
+
+        return {
+            save: save,
+            load: load,
+            list: list
+        };
+
+
+        function save( data ){
+            return $http.post( couchUrl+'/', data ); 
+        }
+
+        function load( id ){
+            return $http.get( couchUrl+'/'+id ).then(function(result){
+                return result.data;
+            });
+        }
+
+        function list(){
+            return $http.get( couchUrl+design+'listByName' ).then(function(results){
+                return results.data.rows.map(function(row){
+                    return row.value;
+                });
+            });
+        }
+    }
+}
